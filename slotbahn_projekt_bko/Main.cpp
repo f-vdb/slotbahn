526f64449335e800a8a8388c7ded30fa22b5e7c4
f-vdb <frank@am-knie.de>
2014-06-14 18:52:29 +0200

    go on

--- slotbahn_projekt_bko/Main.cpp
+++ slotbahn_projekt_bko/Main.cpp
@@ -7,7 +7,6 @@
 #pragma comment(lib, "gdi32.lib")  
 #endif // SFML_STATIC
 
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -17,14 +16,14 @@
 #include <stdlib.h>
 #include <SFML/Graphics.hpp>
 
-
 const std::string BKO_LOGO("BKOLogo_80px.png");
 const size_t WIDTH = 1300;                  // Fensterbreite
-const size_t HEIGHT = 750;					// Fensterhöhe
+const size_t HEIGHT = 750;					// Fensterhoehe
 const size_t PIXEL_LOGO = 80;
 const size_t MARGIN_LOGO = 5;				// Abstand - Logo - Bildschirmrand
 const size_t MARGIN_TOP_QUESTION = 100;		// Abstand - Bildschirmrand oben - Fragentext
 const size_t MARGIN_LEFT_QUESTION = 50;		// Abstadn - Bildschirmrand lings - Fragentext
+const size_t CHARACTER_SIZE = 40;           // Zeichengroesse 
 
 const std::string HS_FILENAME("hs.txt");
 const std::string FOR_FILENAME("for.txt");
@@ -54,7 +53,8 @@ public:
 
 };
 
-enum State {START, HS, FOR, FHR, AHR, STOP, RESTART};
+enum State {START, HS, FOR, FHR, AHR, STOP};
+enum RichtigOderFalsch {NIX ,RICHTIG, FALSCH};
 
 std::vector<Frage> hsAlleFragen;
 std::vector<Frage> forAlleFragen;
@@ -98,6 +98,9 @@ bool leseDateiInVector(const std::string& filename, std::vector<Frage>& vec)
 
 int main()
 {
+	State state = START;
+	RichtigOderFalsch richtigOderFalsch = NIX;
+
 	srand(time(NULL));
 	if (!leseDateiInVector(HS_FILENAME, hsAlleFragen))
 		std::cout << "Fehler: konnte Datei " << HS_FILENAME << " nicht lesen.\n";
@@ -119,17 +122,23 @@ int main()
 	sf::Font font;
 	if (!font.loadFromFile("sansation.ttf"))
 		std::cout << "coud not load font...\n";
-	sf::Text frage;
-	sf::Text antwortA;
-	sf::Text antwortB;
-	sf::Text antwortC;
-	sf::Text state;
-
-	frage.setFont(font);
-	antwortA.setFont(font);
-	antwortB.setFont(font);
-	antwortC.setFont(font);
-	state.setFont(font);
+	
+	sf::Text sfTextStarttext;
+	sf::Text sfTextState;
+	sf::Text sfTextFrage;
+	sf::Text sfTextAntwortA;
+	sf::Text sfTextAntwortB;
+	sf::Text sfTextAntwortC;
+	sf::Text sfTextAuswertung;
+	
+	sfTextStarttext.setFont(font);
+	sfTextState.setFont(font);
+	sfTextFrage.setFont(font);
+	sfTextAntwortA.setFont(font);
+	sfTextAntwortB.setFont(font);
+	sfTextAntwortC.setFont(font);
+	sfTextAuswertung.setFont(font);
+	
 
 	std::string str("Welche Farbe hat Blut?");
 	std::vector<std::string> antworten{ "rot", "grün", "blau" };
@@ -137,56 +146,126 @@ int main()
 	std::string s = str;
 	s += "\n\n\n";
 	
-
-	frage.setColor(sf::Color::Blue);
-	frage.setCharacterSize(40);
+	sfTextStarttext.setCharacterSize(CHARACTER_SIZE);
+	sfTextStarttext.setColor(sf::Color::Black);
+	sfTextState.setCharacterSize(CHARACTER_SIZE);
+	sfTextState.setColor(sf::Color::Black);
+	sfTextFrage.setColor(sf::Color::Blue);
+	sfTextFrage.setCharacterSize(CHARACTER_SIZE);
 	//text.setStyle(sf::Text::Bold | sf::Text::Underlined);
-	antwortA.setColor(sf::Color::Blue);
-	antwortA.setCharacterSize(40);
-	antwortB.setColor(sf::Color::Blue);
-	antwortB.setCharacterSize(40);
-	antwortC.setColor(sf::Color::Blue);
-	antwortC.setCharacterSize(40);
-	state.setCharacterSize(40);
-	state.setColor(sf::Color::Green);
+	sfTextAntwortA.setColor(sf::Color::Blue);
+	sfTextAntwortA.setCharacterSize(CHARACTER_SIZE);
+	sfTextAntwortB.setColor(sf::Color::Blue);
+	sfTextAntwortB.setCharacterSize(CHARACTER_SIZE);
+	sfTextAntwortC.setColor(sf::Color::Blue);
+	sfTextAntwortC.setCharacterSize(CHARACTER_SIZE);
+	sfTextAuswertung.setColor(sf::Color::Magenta);
+	sfTextAuswertung.setCharacterSize(CHARACTER_SIZE);
+	
 
 	
-	state.setPosition(MARGIN_LOGO, MARGIN_LOGO);
-	frage.setPosition(MARGIN_LEFT_QUESTION, MARGIN_TOP_QUESTION);
-	antwortA.setPosition(MARGIN_LEFT_QUESTION, MARGIN_TOP_QUESTION + 20 + 60);
-	antwortB.setPosition(MARGIN_LEFT_QUESTION, MARGIN_TOP_QUESTION + 20 + 60 + 60);
-	antwortC.setPosition(MARGIN_LEFT_QUESTION, MARGIN_TOP_QUESTION + 20 + 60 + 60 + 60);
+	sfTextStarttext.setPosition(MARGIN_LEFT_QUESTION, MARGIN_TOP_QUESTION);
+	sfTextState.setPosition(MARGIN_LOGO, MARGIN_LOGO);
+	sfTextFrage.setPosition(MARGIN_LEFT_QUESTION, MARGIN_TOP_QUESTION);
+	sfTextAntwortA.setPosition(MARGIN_LEFT_QUESTION, MARGIN_TOP_QUESTION + 20 + 60);
+	sfTextAntwortB.setPosition(MARGIN_LEFT_QUESTION, MARGIN_TOP_QUESTION + 20 + 60 + 60);
+	sfTextAntwortC.setPosition(MARGIN_LEFT_QUESTION, MARGIN_TOP_QUESTION + 20 + 60 + 60 + 60);
+	sfTextAuswertung.setPosition(MARGIN_LEFT_QUESTION, MARGIN_TOP_QUESTION + 20 + 60 + 60 + 60 + 60);
 		
+	sfTextStarttext.setString("Drücke Leertaste zum Starten oder zum Neustart");
+	sfTextState.setString("HS");
+	sfTextFrage.setString(s);
+	sfTextAntwortA.setString("A  grün");
+	sfTextAntwortB.setString("B  rot");
+	sfTextAntwortC.setString("C  blau");
+	sfTextAuswertung.setString("RICHTIG  - Lass es krachen...");
 	
-
-	frage.setString(s);
-	antwortA.setString("A  grün");
-	antwortB.setString("B  rot");
-	antwortC.setString("C  blau");
-	state.setString("HS");
 
 	
 
 	while (window.isOpen())
 	{
+		window.clear(sf::Color::White);
+
+		if (state == START)
+		{
+			window.draw(logoSprite);
+			window.draw(sfTextStarttext);
+
+		}
+		else if (state != START)
+		{
+			window.draw(logoSprite);
+			window.draw(sfTextState);
+			window.draw(sfTextFrage);
+			window.draw(sfTextAntwortA);
+			window.draw(sfTextAntwortB);
+			window.draw(sfTextAntwortC);
+			if (richtigOderFalsch == RICHTIG)
+			{
+				sfTextAuswertung.setColor(sf::Color::Green);
+				sfTextAuswertung.setString("RICHTIG  - Lass es krachen...");
+				window.draw(sfTextAuswertung);
+			}
+			else if (richtigOderFalsch == FALSCH)
+			{
+				sfTextAuswertung.setColor(sf::Color::Red);
+				sfTextAuswertung.setString("FALSCH  - Du musst leider warten...");
+				window.draw(sfTextAuswertung);
+			}
+		}
 		
 
-		window.clear(sf::Color::White);
-		window.draw(logoSprite);
-		window.draw(state);
-		window.draw(frage);
-		window.draw(antwortA);
-		window.draw(antwortB);
-		window.draw(antwortC);
-
-	
-		window.display();
-
+		
+		
+		
+		
+		
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			switch (event.type)
+			{
+				case sf::Event::Closed:
+					window.close();
+					break;
+				case sf::Event::KeyPressed:
+					if (event.key.code == sf::Keyboard::Escape)
+					{
+						window.close();
+					}
+					else if (event.key.code == sf::Keyboard::Space)
+					{
+						if (state == START)
+						{
+							state = HS;
+						}
+						else if (state != START)
+						{
+							state = START;
+						}
+					}
+					else if (event.key.code == sf::Keyboard::A)
+					{
+						richtigOderFalsch = FALSCH;
+					}
+					else if (event.key.code == sf::Keyboard::B)
+					{
+						richtigOderFalsch = RICHTIG;
+					}
+					else if (event.key.code == sf::Keyboard::C)
+					{
+						richtigOderFalsch = FALSCH;
+					}
+					break;
+
+
+				default:
+					break;
+
+			}
+			window.display();
+
 		}
 	}
 
